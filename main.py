bc40f75867be2457dbfd23200be18b34baac2e21
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-17 17:43:28 +0700

    handle two types of links in the form

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/update', methods=['POST', 'GET'])
+def update():
+    print("home")
+    return render_template("home.html")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    print("home")
+    return render_template("new.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
